95050a735c2c3ee6da848a736ed5bf2a1a434d55
Agustin Gonzalez <agustingonzalez20150303@gmail.com>
2025-07-03 19:50:40 -0300

    Agrego mas

--- practicaParciales/ParcialEnunPrac.java
+++ practicaParciales/ParcialEnunPrac.java
@@ -1,5 +1,7 @@
 package practicaParciales;
 import practica2.BinaryTree;
+import java.util.List;
+import java.util.LinkedList;
 
 public class ParcialEnunPrac {
     public int sumaImparesPosOrdenMayorA(BinaryTree<Integer> ab , int tope){
@@ -27,29 +29,54 @@ public class ParcialEnunPrac {
         return sumaT;
     }
     
-    public static void main(String[] args) {
+    public List<Integer> resolver (BinaryTree<Integer> ab){
+        List<Integer> lista = new LinkedList<>();
+        if (ab != null && !ab.isEmpty()){
+            resolver(ab,lista);
+        }
+        
+        return lista;
+    }
+    
+    public int resolver (BinaryTree<Integer> ab , List<Integer> lista){
+        int izq = 0; int der = 0;
+        if (ab.isLeaf()){
+            return 0;
+        }         
+        if (ab.hasLeftChild()){
+            izq = resolver(ab.getLeftChild(), lista);
+        }
+            
+        if (ab.hasRightChild()){
+            der = resolver(ab.getRightChild(),lista);
+        }
+        
+        if (izq == der){
+            lista.add(ab.getData());
+        }
+        
+        return 1 + izq + der;
+    }
+    
+ public static void main(String[] args) {
         BinaryTree<Integer> root = new BinaryTree<>(5);
         BinaryTree<Integer> leftChild = new BinaryTree<>(3);
         BinaryTree<Integer> rightChild = new BinaryTree<>(8);
         root.addLeftChild(leftChild);
         root.addRightChild(rightChild);
+
         leftChild.addLeftChild(new BinaryTree<>(1));
         leftChild.addRightChild(new BinaryTree<>(6));
+
         rightChild.addLeftChild(new BinaryTree<>(10));
         rightChild.addRightChild(new BinaryTree<>(11));
 
-        // 
-        //         5
-        //       /   \
-        //      3     8
-        //     / \   /  \
-        //    1   6 10   11
-
-
         ParcialEnunPrac parcial = new ParcialEnunPrac();
-        int tope = 2;
-        int resultado = parcial.sumaImparesPosOrdenMayorA(root, tope);
+        int tope = 2;  
+        int resultadoSuma = parcial.sumaImparesPosOrdenMayorA(root, tope);
+        System.out.println("Suma de los numeros impares mayores que " + tope + " es: " + resultadoSuma);
 
-        System.out.println("Suma de los numeros impares mayores que " + tope + " es: " + resultado);
+        List<Integer> resultadoLista = parcial.resolver(root);
+        System.out.println("Lista de caminos con el mismo número de descendientes en ambos lados: " + resultadoLista);
     }
 }
---
+++ practicaParciales/ParcialProg3T1.java
@@ -0,0 +1,66 @@
+package practicaParciales;
+import practica2.BinaryTree;
+import java.util.List;
+import java.util.LinkedList;
+
+public class ParcialProg3T1 {
+    private BinaryTree<Integer> ab;
+    
+    public ParcialProg3T1(BinaryTree<Integer> arbol){
+        this.ab = arbol;
+    }
+    
+    public List<Integer> procesar(){
+        List<Integer> lista = new LinkedList<>();
+        if (this.ab != null && !this.ab.isEmpty()){
+            procesar(ab,lista);
+        }
+        
+        return lista;
+    }
+    
+    private void procesar (BinaryTree<Integer> ab , List<Integer> lista){
+        if (ab.hasLeftChild() && ab.hasRightChild()){
+            if (ab.getData() % 2 == 0){
+                lista.add(ab.getData());
+            }
+        }
+        
+        if (ab.hasLeftChild()){
+            procesar(ab.getLeftChild(),lista);
+        }
+        if (ab.hasRightChild()){
+            procesar(ab.getRightChild(),lista);
+        }
+    }
+    
+        public static void main(String[] args) {
+        // Construir el árbol de prueba
+        BinaryTree<Integer> root = new BinaryTree<>(6);
+        BinaryTree<Integer> node4 = new BinaryTree<>(4);
+        BinaryTree<Integer> node8 = new BinaryTree<>(8);
+        BinaryTree<Integer> node2 = new BinaryTree<>(2);
+        BinaryTree<Integer> node5 = new BinaryTree<>(5);
+        BinaryTree<Integer> node10 = new BinaryTree<>(10);
+        BinaryTree<Integer> node12 = new BinaryTree<>(12);
+        root.addLeftChild(node4);
+        root.addRightChild(node8);
+        node4.addLeftChild(node2);
+        node4.addRightChild(node5);
+        node8.addLeftChild(node10);
+        node8.addRightChild(node12);
+
+        /*
+                 6
+                / \
+               4   8
+              / \   / \
+             2   5  10  12
+         */
+        ParcialProg3T1 test = new ParcialProg3T1(root);
+        List<Integer> resultado = test.procesar();
+        System.out.println("Nodos que tienen dos hijos y son pares: " + resultado);
+        }
+}
+        
+
